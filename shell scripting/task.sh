7cdb88b4ff2cdb9dbcdffe6b90ae15710b26ef4b
TejasVarshney <tejasvarshney05@gmail.com>
2026-08-18 17:17:55 +0530

    done

--- shell scripting/task.sh
+++ shell scripting/task.sh
@@ -6,3 +6,8 @@ mkdir logs
 cd logs
 ps aux > process.log
 
+read -p "Enter your name: " name
+read -p "Enter your rollno: " rollno
+read -p "Anddd your comment: " comment
+
+echo "My name is $name, rollno: $rollno and your comment is $comment"
