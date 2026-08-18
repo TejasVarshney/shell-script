eb2537b6166d9d5265c74a1219c9f13946ef5bee
TejasVarshney <tejasvarshney05@gmail.com>
2026-08-18 16:47:10 +0530

    first commit

---
+++ shell scripting/hello.sh
@@ -0,0 +1,7 @@
+mkdir hello
+cd hello
+touch app.log
+echo "This is new log" > app.log
+cat app.log
+echo "This is another new log" > app.log
+cat app.log
---
+++ shell scripting/variable.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+variable="Hello, World!"
+echo $variable
+
+#ls="myfile" -- wrong way
+#we can't use commands names as a variable name
+
+name="Tejas"
+rollno = "10313"
+batch="B"
+
+echo "My name is $name"
+echo "Rollno is $rollno"
+echo "Current Batch : $batch" 
